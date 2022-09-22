2fcf33790860f8a4b44ba691d61a2efe1614d3c1
Pedro <pedropequera@gmail.com>
2022-09-22 00:50:47 -0300

    Pagina de login

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,10 @@
 /* Desenvolva seu código aqui... */
+
+const recoveryLink = document.querySelectorAll(`[data-control-modal]`)
+
+for (let i = 0; i < recoveryLink.length; i++){
+    recoveryLink[i].addEventListener(`click`, () => {
+        let modalId = recoveryLink[i].getAttribute(`data-control-modal`)
+        document.getElementById(modalId).classList.toggle(`show-modal`)
+    })
+}
