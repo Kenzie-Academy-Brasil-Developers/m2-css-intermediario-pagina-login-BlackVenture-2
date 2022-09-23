ce02d4639907349afe2161a01b6a8a35bc5430f8
Pedro <pedropequera@gmail.com>
2022-09-23 14:05:46 -0300

    Atualizando pagina de login

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,10 +1,10 @@
 /* Desenvolva seu código aqui... */
 
-const recoveryLink = document.querySelectorAll(`[data-control-modal]`)
+const buttonToogleModal = document.querySelectorAll(`[data-control-modal]`)
 
-for (let i = 0; i < recoveryLink.length; i++){
-    recoveryLink[i].addEventListener(`click`, () => {
-        let modalId = recoveryLink[i].getAttribute(`data-control-modal`)
+for (let i = 0; i < buttonToogleModal.length; i++){
+    buttonToogleModal[i].addEventListener(`click`, () => {
+        let modalId = buttonToogleModal[i].getAttribute(`data-control-modal`)
         document.getElementById(modalId).classList.toggle(`show-modal`)
     })
 }
